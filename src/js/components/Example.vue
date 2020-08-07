6e8035b7c33cd0b59f2476f41f6025e097b65e1f
vertig0 <AlexChubak@gmail.com>
2020-08-07 16:15:02 +0300

    Init

---
+++ src/js/components/Example.vue
@@ -0,0 +1,27 @@
+<template>
+  <section>
+    <div class="container">
+      <img src="/assets/img/logo.jpg" :alt="message" />
+      <p>{{ message }}</p>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: 'Пример Vue component'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.container {
+  text-align: center;
+}
+img {
+  max-width: 200px;
+}
+</style>
